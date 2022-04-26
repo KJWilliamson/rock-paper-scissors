14fd7678c3f89df7986aec566a94899b9e82363c
kamela williamson <kami63116@yahoo.com>
2022-04-26 12:36:58 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+document.querySelector("#check").addEventListener("click", checkWin)
+
+
+function rockPaperScissor() {
+    // Math.random() returns a number between 0 - 1
+    let random = Math.random()
+    if(random <= .33) {
+        return "rock"
+    } else if (random <= .66) {
+        return "paper"
+    } else {
+        return "scissor"
+    }
+}
+rockPaperScissor()
+
+
+function checkWin() {
+
+    let botChoice = rockPaperScissor()
+    let playerChoice = document.querySelector("input").value
+    if((playerChoice === "rock" && botChoice === "scissors") || (playerChoice === "paper" && botChoice === "rock") ||  (playerChoice === "scissors" && botChoice === "paper")) {
+        document.querySelector("#here").innerText = `Player entered ${playerChoice} and computer entered ${botChoice} so 
+PLAYER WINS!!!`;
+    } else if(playerChoice === botChoice) {
+        document.querySelector("#here").innerText = `Player entered ${playerChoice} and computer entered ${botChoice} so 
+You tied!`;
+    } else{
+        document.querySelector("#here").innerText = `Player entered ${playerChoice} and computer entered ${botChoice} so 
+Sorry! You lose!`;
+    }
+    
+}
+
+
